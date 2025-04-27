67299b8e098be6ea2b3edc329be6d1c0e5b081a3
Jenkins CI <jenkins@example.com>
2025-04-27 18:57:32 +0300

    sezginV3

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -2,14 +2,7 @@ package testRunner;
 
 import com.intuit.karate.junit5.Karate;
 
-import static com.intuit.karate.junit5.Karate.*;
-
 public class TestRunner {
-
-    // Tüm testleri çalıştırır
-    @Test
-    Karate testAll() {
-        return run("classpath:feature");  // Burada tag kullanılmaz, tüm testler çalışır.
 
     private static final String FEATURE_PATH = "classpath:feature";
 
@@ -31,6 +24,5 @@ public class TestRunner {
     @Karate.Test
     Karate testAll() {
         return Karate.run(FEATURE_PATH);
-
     }
 }
