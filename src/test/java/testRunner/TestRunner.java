1fb392747124bee5082b28a48ad05ebb18b37174
Jenkins CI <jenkins@example.com>
2025-04-27 18:46:02 +0300

    sezginV1

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -2,29 +2,31 @@ package testRunner;
 
 import com.intuit.karate.junit5.Karate;
 
+import static com.intuit.karate.junit5.Karate.*;
+
 public class TestRunner {
 
     // Sadece @smoke etiketi ile işaretlenmiş testleri çalıştırır
-    @Karate.Test
+    @Test
     Karate testSmoke() {
-        return Karate.run("classpath:feature").tags("@smoke");
+        return run("classpath:feature").tags("@smoke1");
     }
 
     // Sadece @regression etiketi ile işaretlenmiş testleri çalıştırır
-    @Karate.Test
+    @Test
     Karate testRegression() {
-        return Karate.run("classpath:feature").tags("@regression");
+        return run("classpath:feature").tags("@regression");
     }
 
     // @smoke etiketi olmayan testleri çalıştırır
-    @Karate.Test
+    @Test
     Karate testWithoutSmoke() {
-        return Karate.run("classpath:feature").tags("~@smoke");
+        return run("classpath:feature").tags("~@smoke");
     }
 
     // Tüm testleri çalıştırır
-    @Karate.Test
+    @Test
     Karate testAll() {
-        return Karate.run("classpath:feature");  // Burada tag kullanılmaz, tüm testler çalışır.
+        return run("classpath:feature");  // Burada tag kullanılmaz, tüm testler çalışır.
     }
 }
